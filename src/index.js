01841601c55048b9f90e4ba3333a1909d4771781
ritapessoa01 <ritappessoa@gmail.com>
2025-04-01 15:56:22 +0100

    Added the AI API integration and styled the generated recipe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+function displayRecipe(response) {
+  console.log("Recipe generated");
+
+  new Typewriter("#cocktail-recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 10,
+  });
+}
+
 function generateCocktail(event) {
   event.preventDefault();
 
-  new Typewriter("#cocktail-recipe", {
-    strings: "mojito recipe mojito recipe mojito recipe",
-    autoStart: true,
-    cursor: "",
-    delay: 30,
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "43465c40f4c5d3a735b5b8tbed06aoae";
+  let prompt = `User instructions are: Gerenate a recipe for a ${instructionsInput.value} cocktail`;
+  let context =
+    "You are an expert mixologist that loves making cocktails. Your misson is to write a recipe for a cocktail folowing the user instructions in basic HTLM format. Only write a title (in <h2>), the ingredients (in <strong>) and the instructions(in <strong>). Don't write '```html' at the top";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Cocktail recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let cocktailFormElement = document.querySelector("#cocktail-generator-form");
